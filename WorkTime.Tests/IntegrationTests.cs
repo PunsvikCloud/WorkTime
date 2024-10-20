34f2eb20cbbdddb56fa673f75acc4eced4cb6afc
Bjørn Kristian Punsvik <bjorn.kristian.punsvik@tietoevry.com>
2024-10-20 16:00:39 +0200

    feat: initialize WorkTime project with basic API and Docker support

---
+++ WorkTime.Tests/IntegrationTests.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace WorkTime.Tests;
+
+public class IntegrationTests(WebApplicationFactory<Program> factory)
+    : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client = factory.CreateClient();
+
+    [Fact]
+    public async Task Get_Index_ReturnsOk()
+    {
+        // Act
+        var response = await _client.GetAsync("/");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var responseString = await response.Content.ReadAsStringAsync();
+        Assert.Equal("Ok", responseString);
+    }
+}
